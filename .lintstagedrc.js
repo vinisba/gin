25794e8897460c43076133040705cc2e518efb6d
Vinícius Borba <vinisba@gmail.com>
2023-08-02 17:06:41 -0300

    ci: eslint and prettier

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,9 +2,9 @@ const path = require("path");
 
 const buildEslintCommand = (filenames) =>
   `next lint --fix --file ${filenames.map((f) =>
-    path.relative(process.cwd(), f).join(" --file ")
+    path.relative(process.cwd(), f).join(" --file "),
   )}`;
 
 module.exports = {
-  "*.{js,jsx,ts,tsx}": [buildEslintCommand],
+  "./**/*.{js,jsx,ts,tsx,json}": [buildEslintCommand, "prettier --write"],
 };
